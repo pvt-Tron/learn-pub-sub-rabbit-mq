f1fcdf46c7502ecc9324fa19bceecea6917187b7
pvt-Tron <nickpvt@yahoo.it>
2026-06-15 19:08:54 +0200

    XD: QUEUE_commit

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,34 @@
+import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril game server connected to RabbitMQ!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
+
+  const publishCh = await conn.createConfirmChannel();
+
+  try {
+    await publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
+      isPaused: true,
+    });
+  } catch (err) {
+    console.error("Error publishing message:", err);
+  }
 }
 
 main().catch((err) => {
